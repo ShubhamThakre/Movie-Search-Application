7d4869a32aabebe19a036a39cb74f8c63ebde046
Shubham Thakare <sthakare@anant.saama.com>
2019-11-24 18:35:26 +0530

    Intial Setup done. Moving forward to remaking of UI and functionality

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Header from '../components/header'
+import Layout from '../components/layout'
+
+
+const Index = () => (
+    <Layout> 
+      <p>I am about page.</p>
+    </Layout>
+  )
+  
+export default Index
---
+++ pages/index.js
@@ -0,0 +1,16 @@
+// This is the Link API
+import Link from 'next/link';
+import Header from '../components/header'
+import Layout from '../components/layout'
+
+
+
+const Index = () => {
+  return(
+      <Layout>
+        <p>Hello Next.js</p>
+      </Layout>
+  );
+}
+  
+export default Index
